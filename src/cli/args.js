cec78fabfcf7f215c509fa74d7eeb30326b0624b
Olga <deirin@yandex.ru>
2022-06-03 00:18:54 +0300

    feat: add cli.parseArgs

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,3 +1,10 @@
 export const parseArgs = () => {
-    // Write your code here 
+  const args = process.argv;
+  for (let key in args) {
+    if (args[key].slice(0, 2) === '--') {
+      console.log(args[key].slice(2), 'is', args[String(Number(key) + 1)]);
+    }
+  }
 };
+parseArgs();
+//node src/cli/args --propName 111 --prop2Name 222
