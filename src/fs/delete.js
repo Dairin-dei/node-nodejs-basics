34252ab044af681ca181a1063acb232c5720549e
Olga <deirin@yandex.ru>
2022-06-02 23:25:11 +0300

    feat: add delete

--- src/fs/delete.js
+++ src/fs/delete.js
@@ -1,3 +1,23 @@
+import { access, rm } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const remove = async () => {
-    // Write your code here 
+  const fileName = fileURLToPath(import.meta.url);
+  const dirName = dirname(fileName);
+  const currentFile = join(dirName, 'files', 'fileToRemove.txt');
+
+  access(currentFile, (err) => {
+    if (err === 'ENOENT') {
+      throw Error('FS operation failed');
+    }
+
+    rm(currentFile, { recursive: true }, (err) => {
+      if (err) {
+        throw Error(err.message);
+      }
+    });
+  });
 };
+
+remove();
