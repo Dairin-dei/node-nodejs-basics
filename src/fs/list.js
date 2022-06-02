706dd18274f0b9f8c45bf146907bcb913652baa4
Olga <deirin@yandex.ru>
2022-06-02 23:31:01 +0300

    feat: add list

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,3 +1,22 @@
+import { access, readdir } from 'fs';
+import { join, dirname } from 'path';
+import { fileURLToPath } from 'url';
+
 export const list = async () => {
-    // Write your code here 
+  const dirName = dirname(fileURLToPath(import.meta.url));
+  const currentPath = join(dirName, 'files');
+  access(currentPath, (err) => {
+    if (err && err.code === 'ENOENT') {
+      throw Error('FS operation failed');
+    }
+    readdir(currentPath, { withFileTypes: true }, (err, files) => {
+      if (err) {
+        throw Error(err.message);
+      }
+      files.forEach((file) => {
+        console.log(file.name);
+      });
+    });
+  });
 };
+list();
