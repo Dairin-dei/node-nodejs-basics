a75ea59a7ae827b8dfdf76d0475ae29d70eb7bf9
Olga <deirin@yandex.ru>
2022-06-02 22:55:51 +0300

    feat: add fs.copy

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,3 +1,72 @@
+import { access, copyFile, mkdir, readdir, rm } from "fs";
+import { join, dirname } from "path";
+import { fileURLToPath } from "url";
+
 export const copy = async () => {
-    // Write your code here 
+  const fileName = fileURLToPath(import.meta.url);
+  const destinationFolder = join(dirname(fileName), "files_copy");
+  const sourceFolder = join(dirname(fileName), "files");
+
+  const copyFolder = async (previousPath, futurePath) => {
+    readdir(previousPath, { withFileTypes: true }, (err, files) => {
+      files.forEach((file) => {
+        if (file.isDirectory()) {
+          const newPath = join(futurePath, file.name);
+          mkdir(newPath, { recursive: true }, (err) => {
+            if (err) {
+              throw err;
+            }
+            copyFolder(
+              join(previousPath, file.name),
+              join(futurePath, file.name)
+            );
+          });
+        } else {
+          copyFile(
+            join(previousPath, file.name),
+            join(futurePath, file.name),
+            (err) => {
+              if (err) {
+                throw Error(err.message);
+              }
+            }
+          );
+        }
+      });
+    });
+  };
+
+  access(sourceFolder, (err) => {
+    if (err && err.code === "ENOENT") {
+      throw Error("Source folder doesn't exists");
+    }
+    access(destinationFolder, (err) => {
+      if (err && err.code === "ENOENT") {
+        mkdir(destinationFolder, { recursive: true }, (err) => {
+          if (err) {
+            throw err;
+          }
+          copyFolder(sourceFolder, destinationFolder);
+        });
+      } else {
+        /* rm(destinationFolder, { recursive: true }, (err) => {
+          if (err) {
+            throw Error(err.message);
+          }
+          mkdir(
+            join(destinationFolder, "files"),
+            { recursive: true },
+            (err) => {
+              if (err) {
+                throw err;
+              }
+              copyFolder(sourceFolder, join(destinationFolder, "files"));
+            }
+          );
+        }); */ //remove
+        throw Error("Destination folder already exists"); //remove
+      }
+    });
+  });
 };
+copy();
