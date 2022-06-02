5d03ce7b73514b391238b436f4412bc817bfd46f
Olga <deirin@yandex.ru>
2022-06-02 23:37:40 +0300

    feat: add fs.read

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,3 +1,20 @@
+import { open, readFile } from 'fs';
+import { dirname, join } from 'path';
+import { fileURLToPath } from 'url';
+
 export const read = async () => {
-    // Write your code here 
+  const dirName = dirname(fileURLToPath(import.meta.url));
+  const fileName = join(dirName, 'files', 'fileToRead.txt');
+  open(fileName, (err) => {
+    if (err && err.code === 'ENOENT') {
+      throw Error('FS operation failed');
+    }
+    readFile(fileName, (err, data) => {
+      if (err) {
+        throw Error(err.message);
+      }
+      console.log(data);
+    });
+  });
 };
+read();
