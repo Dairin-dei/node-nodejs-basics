fdda41a2145d4b12bffad33aae0097d84a3236be
Olga <deirin@yandex.ru>
2022-06-04 01:37:43 +0300

    feat: add zlib.decompress, change zlib.compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -2,14 +2,20 @@ import { createReadStream, createWriteStream } from 'fs';
 import { createGzip } from 'zlib';
 import { fileURLToPath } from 'url';
 import { join, dirname } from 'path';
+import { pipeline } from 'stream';
 
 export const compress = async () => {
   const dirName = dirname(fileURLToPath(import.meta.url));
   const file = join(dirName, 'files', 'fileToCompress.txt');
   const outputFile = join(dirName, 'files', 'archive.gz');
+
   const gZip = createGzip();
   const readStream = createReadStream(file, 'utf-8');
   const writeStream = createWriteStream(outputFile);
-  readStream.pipe(gZip).pipe(writeStream);
+  pipeline(readStream, gZip, writeStream, (err) => {
+    if (err) {
+      throw Error(err.message);
+    }
+  });
 };
 compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,3 +1,24 @@
+import { createReadStream, createWriteStream, open } from 'fs';
+import { createGunzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
 export const decompress = async () => {
-    // Write your code here 
+  const dirName = dirname(fileURLToPath(import.meta.url));
+
+  const file = join(dirName, 'files', 'archive.gz');
+  const outputFile = join(dirName, 'files', 'fileToCompress1.txt');
+
+  open(outputFile, (err) => {
+    if (err === null) {
+      throw Error('File already exists');
+    }
+    if (err) {
+      const unZip = createGunzip();
+      const readStream = createReadStream(file);
+      const writeStream = createWriteStream(outputFile);
+      readStream.pipe(unZip).pipe(writeStream);
+    }
+  });
 };
+decompress();
