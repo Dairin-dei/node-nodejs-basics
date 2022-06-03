a300a5c224e3a0354657dc78ccae1f1d36ad434a
Olga <deirin@yandex.ru>
2022-06-04 01:07:49 +0300

    refactor: change file name in zlib.compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -6,7 +6,7 @@ import { join, dirname } from 'path';
 export const compress = async () => {
   const dirName = dirname(fileURLToPath(import.meta.url));
   const file = join(dirName, 'files', 'fileToCompress.txt');
-  const outputFile = join(dirName, 'files', 'fileToCompress.txt.gz');
+  const outputFile = join(dirName, 'files', 'archive.gz');
   const gZip = createGzip();
   const readStream = createReadStream(file, 'utf-8');
   const writeStream = createWriteStream(outputFile);
