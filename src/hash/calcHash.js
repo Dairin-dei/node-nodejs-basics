55f8a7c0d1d9dcf6f993ec6c806d4164a288480e
Olga <deirin@yandex.ru>
2022-06-03 23:19:58 +0300

    feat: add hash

--- src/hash/calcHash.js
+++ src/hash/calcHash.js
@@ -1,3 +1,20 @@
+import { open, readFile } from 'fs';
+import { join, dirname, resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { createHash } from 'crypto';
+
 export const calculateHash = async () => {
-    // Write your code here 
+  const file = join(
+    dirname(
+      fileURLToPath(import.meta.url),
+      'files',
+      'fileToCalculateHashFor.txt'
+    )
+  );
+
+  const value = createHash('sha256').update(file).digest('hex');
+
+  return value;
 };
+
+console.log(await calculateHash());
