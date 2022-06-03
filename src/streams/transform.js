2f8b4ca8b58ee54462ce0eea925a2d6a273e0a44
Olga <deirin@yandex.ru>
2022-06-04 00:29:01 +0300

    feat: add streams.transform

--- src/streams/transform.js
+++ src/streams/transform.js
@@ -1,3 +1,20 @@
+import { Transform, pipeline } from 'stream';
+import { createInterface } from 'readline';
+
 export const transform = async () => {
-    // Write your code here 
+  const input = process.stdin;
+  const output = process.stdout;
+
+  const transformLine = new Transform({
+    transform(line, encr, callback) {
+      this.push(line.toString().trim().split('').reverse().join('') + '\n');
+      callback();
+    },
+  });
+
+  console.log('print smth pls');
+  pipeline(input, transformLine, output, (err) => {
+    throw Error(err.message);
+  });
 };
+transform();
