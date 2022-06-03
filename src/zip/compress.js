87157a71cb933a9edcbe328dc90ffa3b15545117
Olga <deirin@yandex.ru>
2022-06-04 00:59:36 +0300

    feat: add zlib.compress

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,3 +1,16 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { pipeline } from 'stream';
+import { createGzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import { join, dirname } from 'path';
+
 export const compress = async () => {
-    // Write your code here 
+  const dirName = dirname(fileURLToPath(import.meta.url));
+  const file = join(dirName, 'files', 'fileToCompress.txt');
+  const outputFile = join(dirName, 'files', 'fileToCompress.txt.gz');
+  const gZip = createGzip();
+  const readStream = createReadStream(file, 'utf-8');
+  const writeStream = createWriteStream(outputFile);
+  readStream.pipe(gZip).pipe(writeStream);
 };
+compress();
