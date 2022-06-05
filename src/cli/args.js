c05e1d1bf05f6067f939b241edbd313e04c85c73
Olga <deirin@yandex.ru>
2022-06-05 17:32:23 +0300

    refactor: change input in cli.args

--- src/cli/args.js
+++ src/cli/args.js
@@ -1,10 +1,9 @@
 export const parseArgs = () => {
   const args = process.argv;
   for (let key in args) {
-    if (args[key].slice(0, 2) === '--') {
-      console.log(args[key].slice(2), 'is', args[String(Number(key) + 1)]);
+    if (args[key].slice(0, 2) === "--") {
+      console.log(args[key].slice(2), "is", args[String(Number(key) + 1)]);
     }
   }
 };
 parseArgs();
-//node src/cli/args --propName 111 --prop2Name 222
